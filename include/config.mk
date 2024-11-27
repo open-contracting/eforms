4932bc38347fa86da906d32adc1e282bb84db53d
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-11-26 20:06:13 -0500

    fix: Configure PDF

--- include/config.mk
+++ include/config.mk
@@ -19,6 +19,8 @@ DIST_FILES=schema/profile/release-schema.json schema/profile/codelists schema/pa
 POT_DIR=$(BUILD_DIR)/locale
 # The prefix, if any, to the schema and codelists domains.
 DOMAIN_PREFIX=
+# The Transifex organization name.
+TRANSIFEX_ORGANIZATION=open-contracting-partnership-1
 # The Transifex project name.
 TRANSIFEX_PROJECT=
 # Any additional extract targets.
@@ -27,9 +29,9 @@ EXTRACT_TARGETS=
 SPHINX_AUTOBUILD_EXTRA_ARGS=--re-ignore patched
 
 # The path to the branch of the documentation to print to PDF.
-PDF_ROOT=
+PDF_ROOT=/profiles/eforms/latest
 # The pattern of pages to print to PDF. Update if the documentation adds, removes or renames pages.
-PDF_PAGES={}
+PDF_PAGES={,primer/,how/,mapping/,codelist/{,buyer-contracting-type/,dps-usage/,environmental-impact/,form-type/,framework-agreement-method/,gpp-criteria/,index/,innovative-acquisition/,number-fixed/,number-threshold/,number-weight/,organisation-role/,procurement-procedure-type/,received-submission-type/,selection-criterion/,social-objective/,strategic-procurement/},operations/,reference/{,schema/,codelists/},changelog/}
 # 15000 may warn: "Warning: Received createRequest signal on a disposed ResourceObject's NetworkAccessManager. This might
 # be an indication of an iframe taking too long to load."
 PDF_DELAY=20000
